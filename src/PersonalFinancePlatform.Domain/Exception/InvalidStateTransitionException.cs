58f32a2b03478f3e9a964ecdea5ca493fb6828c6
Freezinggg <blerubs@gmail.com>
2026-07-18 13:17:51 +0700

    feat(domain): add User Entity and common exception class

---
+++ src/PersonalFinancePlatform.Domain/Exception/InvalidStateTransitionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinancePlatform.Domain.Exception
+{
+    public sealed class InvalidStateTransitionException : DomainException
+    {
+        public InvalidStateTransitionException(string message)
+            : base(message, FailureCategory.State)
+        {
+        }
+    }
+}
---
+++ src/PersonalFinancePlatform.Domain/User/User.cs
@@ -0,0 +1,52 @@
+﻿using PersonalFinancePlatform.Domain.Exception;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalFinancePlatform.Domain.User
+{
+    public sealed class User
+    {
+        public Guid Id { get; }
+        public string Email { get; private set; } 
+        public string DisplayName { get; private set; }
+        public string PasswordHash { get; private set; }
+        public DateTime CreatedAt { get; }
+
+
+        public User(Guid id, string email, string displayName, string passwordHash, DateTime createdAt)
+        {
+            if(id == Guid.Empty)
+                throw new InvariantViolationException("[User ID] cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new InvariantViolationException("[Email] cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(displayName))
+                throw new InvariantViolationException("[Display Name] cannot be empty.");
+
+            if (string.IsNullOrWhiteSpace(passwordHash))
+                throw new InvariantViolationException("[Password] cannot be empty.");
+
+            Id = id;
+            Email = email;
+            DisplayName = displayName;
+            PasswordHash = passwordHash;
+            CreatedAt = createdAt;
+        }
+
+
+        public void ChangeDisplayName(string newDisplayName)
+        {
+            if (string.IsNullOrWhiteSpace(newDisplayName))
+                throw new InvariantViolationException("[Display Name] cannot be empty.");
+
+            DisplayName = newDisplayName;
+        }
+
+        public void ChangePassword(string newPasswordHash)
+        {
+            PasswordHash = newPasswordHash;
+        }
+    }
+}
